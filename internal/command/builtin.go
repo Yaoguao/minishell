e4a1ab49624c1772770dcc7e02fb0b4ba89d12c2
Yaoguao <glenioJ@yandex.ru>
2025-10-06 20:57:50 +0400

    add echo command

--- internal/command/builtin.go
+++ internal/command/builtin.go
@@ -1,9 +1,10 @@
-package commands
+package command
 
 import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 type BuiltinCommand struct {
@@ -20,6 +21,20 @@ func (b *BuiltinCommand) Run(input io.Reader, output io.Writer) error {
 		return os.Chdir(b.Args[0])
 	case "exit":
 		os.Exit(0)
+	case "echo":
+		for i, arg := range b.Args {
+			if strings.HasPrefix(arg, "$") && len(arg) > 1 {
+				val := os.Getenv(arg[1:])
+				fmt.Fprint(output, val)
+			} else {
+				fmt.Fprint(output, arg)
+			}
+			if i < len(b.Args)-1 {
+				fmt.Fprint(output, " ")
+			}
+		}
+		fmt.Fprintln(output)
+		return nil
 	default:
 		return fmt.Errorf("unknown builtin: %s", b.Name)
 	}
